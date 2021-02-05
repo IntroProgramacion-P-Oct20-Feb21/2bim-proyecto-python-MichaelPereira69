7d37de9207d4c8bbb3f78d0134c09db4fc0e467c
Michael Pereira <michaelmaldonadopereira69@gmail.com>
2021-02-05 11:11:58 -0500

    Trabajo Arreglado

--- codigo/run.py
+++ codigo/run.py
@@ -1,20 +1,13 @@
-"""
-    Proyecto Bimestral
-    Segundo Bimestre
-
-    Problemática:
-"""
 def crearFacebook():
     print("Creemos una cuenta de Facebook ")
     usuario = input("Nombre de usuario\n> ")
-    edad = int(input("Edad\n> "))
+    edad = float (input("Edad\n> "))
     ciudad = input("Ciudad en la que resides\n> ")
     pais = input("País\n> ")
     correo = input("Correo electrónico\n> ")
     cadena = (f"Tus datos son:\n"
               f"Usuario: {usuario}\nEdad: {edad}\nCiudad: {ciudad}\n"
-              f"País: {pais}\nCorreo Electrónico: {correo}\n"
-              f"------------------")
+              f"País: {pais}\nCorreo Electrónico: {correo}\n")
     return cadena
 
 def crearTwitter():
@@ -29,8 +22,7 @@ def crearTwitter():
     print(f"Tus datos personales son: \n"
           f"Usuario: {usuario}\nApellidos: {apellidos}\nEdad: {edad}\n"
           f"Ciudad: {ciudad}\nPaís: {pais}\nIdioma: {idioma}\n"
-          f"Correo Electrónico: {correo}\n"
-          f"-----------------------")
+          f"Correo Electrónico: {correo}\n")
 
 
 def crearWhatsapp():
@@ -43,7 +35,7 @@ def crearWhatsapp():
     cadena = (f"Tus Datos son:\n"
               f"Usuario: {usuario}\nNúmero de teléfono: {numero}\n"
               f"Edad: {edad}\nCiudad: {ciudad}\nPaís: {pais}\n"
-              f"------------------")
+              f"-----------")
     return cadena
 
 
@@ -56,8 +48,7 @@ def crearTelegram():
     area_interes = input("Escrube un Area de tu interés\n> ")
     print(f"Tus datos personales son:\n"
           f"Usuario: {usuario}\nNúmero de teléfono: {numero}\n"
-          f"Ciudad: {ciudad}\nPaís: {pais}\nÁrea de Interés: {area_interes}\n"
-          f"*--------------------")
+          f"Ciudad: {ciudad}\nPaís: {pais}\nÁrea de Interés: {area_interes}\n")
 
 
 def crearSignal():
@@ -69,8 +60,7 @@ def crearSignal():
     hobby = input("Hobby principal\n> ")
     cadena = (f"Tus datos personales son: \n"
               f"Usuario: {usuario}\nNúmero de teléfono: {numero}\n"
-              f"Ciudad: {ciudad}\nPaís: {pais}\nHobby principla: {hobby}\n"
-              f"---------------")
+              f"Ciudad: {ciudad}\nPaís: {pais}\nHobby principla: {hobby}\n")
     return cadena
 
 
@@ -82,17 +72,15 @@ def crearInstagram():
     correo = input("Ingrese su correo electrónico\n> ")
     print(f"Tus datos personales son: \n"
           f"Usuario: {usuario}\nCiudad: {ciudad}\ndad: {edad}\n"
-          f"Correo Electrónico: {correo}\n"
-          f"---------------")
+          f"Correo Electrónico: {correo}\n")
 
 
 def crearFlickr():
-    print("Creemos una cuenta de Flickr --*")
+    print("Creemos una cuenta de Flickr *")
     usuario = input("Nombre de usuario\n> ")
     correo = input("Correo electrónico\n> ")
     cadena = (f" Datos Ingresados\n"
-              f"Usuario: {usuario}\nCorreo Electrónico: {correo}\n"
-              f"----------------")
+              f"Usuario: {usuario}\nCorreo Electrónico: {correo}\n")
     return cadena
 
 
@@ -115,7 +103,6 @@ if __name__ == "__main__":
     bandera = True
     contador = 0
     while bandera:
-        contador += 1
         print("Opciones")
         opcion = int(input("> Inserte 1 para creara una cuenta de Facebook.\n"
                            + "> Ingrese 2 para creara una cuenta de Twitter.\n"
@@ -123,27 +110,32 @@ if __name__ == "__main__":
                            + "> Ingrese 4 para creara una cuenta de Telegram.\n"
                            + "> Ingrese 5 para creara una cuenta de Signal.\n"
                            + "> Ingrese 6 para creara una cuenta de Instagram.\n"
-                           + "> Ingrese 7 para creara una cuenta de Flickr.\n"
-                           + "------------------\n> "))
+                           + "> Ingrese 7 para creara una cuenta de Flickr.\n"))
         if (opcion == 1):
+            contador += 1
             cuenta_facebook = crearFacebook()
             print(cuenta_facebook)
         else:
             if (opcion == 2):
+                contador += 1
                 crearTwitter()
             else:
                 if (opcion == 3):
+                    contador += 1
                     cuenta_whatsapp = crearWhatsapp()
                     print(cuenta_whatsapp)
                 else:
                     if (opcion == 4):
+                        contador += 1
                         crearTelegram()
                     else:
                         if (opcion == 5):
+                            contador += 1
                             cuenta_signal = crearSignal()
                             print(cuenta_signal)
                         else:
                             if (opcion == 6):
+                                contador += 1
                                 crearInstagram()
                             else:
                                 if (opcion == 7):
@@ -151,7 +143,8 @@ if __name__ == "__main__":
                                     print(cuenta_flickr)
                                 else:
                                     print("Opción incorrecta")
-        salida = input("Ingrese 'si' si desea salir del ciclo\n> ")
-        if (salida == "si"):
+        salida = input("Ingrese 'salir' si desea salir del ciclo o diguite"
+                       " 1 para crear otra cuenta\n> ")
+        if (salida == "salir"):
             bandera = False
     print(obtenerMensaje(contador))
